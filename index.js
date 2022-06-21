0b3634039d2a93a686d85cdd6666bc23e4546885
Javier <78636781+JavierLoera@users.noreply.github.com>
2022-06-21 18:52:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ dotenv.config();
 app.use(express.json());
 app.use("/auth", authRouter);
 
-app.post("/api/articles/:name/add-comments", (req, res) => {
-  const { username, text } = req.body;
-  const articleName = req.params.name;
-  articlesInfo[articleName].comments.push({ username, text });
-  res.status(200).send(articlesInfo);
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("😍😍 http://localhost:" + PORT));
